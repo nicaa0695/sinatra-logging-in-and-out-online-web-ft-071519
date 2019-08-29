d0882b73a9d7820e9109356e0aa5739877d3e71b
Veronica Lopez <nicaa0695@gmail.com>
2019-08-29 18:57:42 +0000

    Automatically backed up by Learn

--- db/migrate/20190829185559_users.rb
+++ db/migrate/20190829185559_users.rb
@@ -3,6 +3,6 @@ class Users < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.integer :balance
+      t.decimal :balance
   end
 end
